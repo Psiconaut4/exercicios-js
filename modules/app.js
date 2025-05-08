ab6054d4d58c1549c843e72e067e4165f31bddee
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2025-05-08 12:46:17 -0300

    modules exports

---
+++ modules/app.js
@@ -0,0 +1,9 @@
+var somar = require('./soma');
+var subtrair = require('./sub');
+var multiplicar = require('./multi');
+var dividir = require('./div')
+
+console.log(somar(10, 10));
+console.log(subtrair(10, 5));
+console.log(multiplicar(10, 5));
+console.log(dividir(10, 5));
