4ffd82304a4ce201ebb573614ca47f7c9e7aed6c
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2025-05-14 14:49:21 -0300

    Brincando com express

--- server/express.js
+++ server/express.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/home.html");
+})
 
+app.get("/aipapai", function(req,res){
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.get("/ola/:nome/:cargo/:tamanho", function(req,res){
+    res.send("ola "+req.params.nome+" do penis levemente "+"<h1>"+req.params.tamanho+"</h1>"+"<h2> seu cargo é "+req.params.cargo+"</h2>");
+})
 
 app.listen(8081, function(){
-    console.log('Ta rodando na url http://localhost:8081');
+    console.log('Ta rodando na url http://localhost:8081 carai');
 });
