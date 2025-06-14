e3c3f50815f766af5836c8d99254e0a15ef1fd7f
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2025-06-14 16:20:29 -0300

    express

--- server/express/index.js
+++ server/express/index.js
@@ -5,7 +5,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 // config
     //template engine
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
 
 // conexão com o banco de dados
@@ -13,7 +13,15 @@ const sequelize = new Sequelize('teste', 'root', '' , {
     host: 'localhost',
     dialect: 'mariadb'
 });
+//Rotas
 
+app.get('/cad', function(req, res){
+    res.render('formulario');
+});
+
+app.post('/add', function(req, res){
+    res.send('Formulário recebido com sucesso!');
+});
 
 app.listen(6069, function(){
     console.log('Rodando na http://localhost:6069');
